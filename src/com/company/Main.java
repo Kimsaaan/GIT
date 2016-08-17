93a8c74c8ba03f12c2744ee2046c81ce17bed566
Stevelicious <Stevelicious@users.noreply.github.com>
2016-08-17 13:47:03 +0200

    Ändrat stavfel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Kimsan");
-        System.out.println("Är bäst");
+        System.out.println("Är sämst");
 
 
 
